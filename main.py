0fd144593008bd728767aa5e13415b136e587165
Sharon Lin <sharon.lin.14@gmail.com>
2018-07-09 21:23:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class Block(Resource):
     def get(self):
         global coins
         mined = randint(1, 1000000)*0.01
+        if mined < 7000:
+           return "Mining... Please wait."
         coins += mined
         return "You have mined {} satoshi. There are currently {} satoshi.".format(mined, coins), 200
 
